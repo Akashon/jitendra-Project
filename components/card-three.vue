bd70fee5a58a29f51972b0935bf70a57def97565
akashon <akash.mokhle@visko.group>
2024-11-14 12:02:03 +0530

    update latest

--- components/card-three.vue
+++ components/card-three.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="product-card product-card--layout--grid pb-2" style="width:18rem;">
-        <div class="product-card__actions-list">
+        <!-- <div class="product-card__actions-list">
             <button class="product-card__action product-card__action--quickview" type="button" aria-label="Quick view">
                 <i class="fa-solid fa-expand"></i>
             </button>
@@ -10,13 +10,11 @@
                 <i class="fa-solid fa-chart-simple" style="width: 16px ; height: 16px;"></i>
             </button>
 
-        </div>
+        </div> -->
         <div class="product-card__image">
             <div class="product-card__image">
-                <div class="image image--type--product"><a :href="`/product/`" class="image__body"><img
-                            class="image__tag"
-                            :src="image"
-                            alt=""></a>
+                <div class="image image--type--product"><a :href="`/product/${id}/`" class="image__body"><img
+                            class="image__tag" :src="image" alt=""></a>
                 </div>
             </div>
 
@@ -24,7 +22,7 @@
         <div class="product-card__info">
             <div class="product-card__meta"><span class="product-card__meta-title">SKU:</span> 573-23743-C</div>
             <div class="product-card__name">
-                <div><a href="product-full.html">{{ name }}</a>
+                <div><a href="`/product/${id}/`">{{ name }}</a>
                 </div>
             </div>
 
@@ -40,10 +38,10 @@ export default {
         }
     },
     props: {
-        id: { default: ''  },
-        image: { default: ''  },
-        name: { default: ""  },
-        rDescription: {  default: "" },
+        id: { default: '' },
+        image: { default: '' },
+        name: { default: "" },
+        rDescription: { default: "" },
     }
 }
 </script>
